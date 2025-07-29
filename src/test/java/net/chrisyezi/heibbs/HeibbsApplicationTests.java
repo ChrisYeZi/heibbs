aff0ec8c6344d733b5f43824fa561ba7efaa0ce0
ChrisYeZi <1812304293@qq.com>
2025-07-29 16:06:06 +0800

    初始化

---
+++ src/test/java/net/chrisyezi/heibbs/HeibbsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chrisyezi.heibbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeibbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
