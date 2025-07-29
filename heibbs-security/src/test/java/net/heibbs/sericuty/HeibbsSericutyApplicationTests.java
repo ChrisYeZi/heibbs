269f5a11f4d5a5cee0f139a899d5f40eafd0a76a
ChrisYeZi <1812304293@qq.com>
2025-07-29 17:01:57 +0800

    添加security模块

---
+++ heibbs-security/src/test/java/net/heibbs/sericuty/HeibbsSericutyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.heibbs.sericuty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeibbsSericutyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
